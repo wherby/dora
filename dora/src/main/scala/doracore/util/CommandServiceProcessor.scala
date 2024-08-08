4cdebecb83320bfd235575ca21fb397bb59f7991
wherby <187225577@qq.com>
2024-08-08 23:15:13 +0800

    add copy right header

--- dora/src/main/scala/doracore/util/CommandServiceProcessor.scala
+++ dora/src/main/scala/doracore/util/CommandServiceProcessor.scala
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2024 [Tao Zhou](187225577@qq.com). - All Rights Reserved
+ *
+ * Unauthorized copying or redistribution of this file in source and binary forms via any medium
+ * is strictly prohibited.
+ */
+
 package doracore.util
 
 import java.io.{ByteArrayOutputStream, PrintWriter}
--- dora/src/main/scala/doradilla/conf/DoraConf.scala
+++ dora/src/main/scala/doradilla/conf/DoraConf.scala
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2024 [Tao Zhou](187225577@qq.com). - All Rights Reserved
+ *
+ * Unauthorized copying or redistribution of this file in source and binary forms via any medium
+ * is strictly prohibited.
+ */
+
 package doradilla.conf
 
 import com.typesafe.config.{Config, ConfigFactory}
